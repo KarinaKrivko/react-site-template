9f2850a75f5839c27c20aaaeb30efa2e83ffa900
KarinaK <mskarinakrivko@gmail.com>
2022-11-09 23:55:25 +0100

    initial commit

---
+++ site_template/src/components/App/index.jsx
@@ -0,0 +1,14 @@
+import '../../style.css'
+import Nav from '../Nav';
+import Services from '../Services';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Services />
+    </div>
+  );
+}
+
+export default App;
