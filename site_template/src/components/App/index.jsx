93ff84ecb138e2143e7dae2065153ba5ad2c5dd0
KarinaK <mskarinakrivko@gmail.com>
2022-11-11 11:08:57 +0100

    changes added

--- site_template/src/components/App/index.jsx
+++ site_template/src/components/App/index.jsx
@@ -1,12 +1,14 @@
 import '../../style.css'
 import Nav from '../Nav';
 import Services from '../Services';
+import AboutUs from '../AboutUs';
 
 function App() {
   return (
     <div>
       <Nav />
       <Services />
+      <AboutUs />
     </div>
   );
 }
